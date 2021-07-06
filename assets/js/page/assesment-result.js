c35e207bb5b0bccb70121d87e536d7f63b91dd69
qolbudr <qolbu.dzikru30@gmail.com>
2021-07-06 21:37:29 +0700

    Fix assesment input twice

--- assets/js/page/assesment-result.js
+++ assets/js/page/assesment-result.js
@@ -23,25 +23,24 @@ $("#activity").text(activity + '%');
 $("#calories").text(calories + ' Kal');
 $("#bmi-sub").text(bmi_sub);
 
-db.collection("assesment").doc(dbUser.uid).set({
-  weight: result.weight,
-  height: result.height,
-  age: result.age,
-  activity: result.activity,
-  gender: result.gender,
-  bmi: bmi,
-  bbi: bbi,
-  precentage_activity: activity,
-  calories: calories
-})
-.then(() => {
-    localStorage.removeItem('dataAssesment');
-})
-.catch((error) => {
-    swal({title: "Error", text: error, icon: "error", buttons: { hapus: "OK" }})
-});
-
 $(".btn-home").click(function() {
-	Router.navigate('home')
+	db.collection("assesment").doc(dbUser.uid).set({
+	  weight: result.weight,
+	  height: result.height,
+	  age: result.age,
+	  activity: result.activity,
+	  gender: result.gender,
+	  bmi: bmi,
+	  bbi: bbi,
+	  precentage_activity: activity,
+	  calories: calories
+	})
+	.then(() => {
+	    localStorage.removeItem('dataAssesment');
+	    Router.navigate('home');
+	})
+	.catch((error) => {
+	    swal({title: "Error", text: error, icon: "error", buttons: { hapus: "OK" }})
+	});
 })
 
